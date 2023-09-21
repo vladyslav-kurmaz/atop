5872cf7cf0239b90fe9b2f7dd2dea0f7b4be16e3
vladyslav-kurmaz <posolkoronovan@gmail.com>
2023-09-21 23:03:37 +0300

    fix bug with words in patent and certificate

--- app/qualitySystem/certificate/page.tsx
+++ app/qualitySystem/certificate/page.tsx
@@ -7,7 +7,7 @@ export default function Certificate() {
     <div className="certificate">
       <h1 className="certificate__title">Certificates</h1>
       <p className="certificate__text">
-        The implementation of the quality management system guarantees that the quality of the manufactured products is stable and focused on a specific consumer, and increases the company's competitiveness.
+        The implementation of the quality management system guarantees that the quality of the manufactured products is stable and focused on a specific consumer, and increases the company{`'`}s competitiveness.
       </p>
 
       <CertificatesAndPatents status={false}/>
--- app/qualitySystem/patents/page.tsx
+++ app/qualitySystem/patents/page.tsx
@@ -7,7 +7,7 @@ export default function Patents() {
     <div className="patents">
       <h1 className="patents__title">Patents</h1>
       <p className="patents__text">
-        One of the directions of the company's activity is research and development work on the search for the latest technologies and materials, which are embodied in the design of products. A lot of own developments of A.T.O.R. NGO LLC protected by a number of patents in the field of passenger rail transport.
+        One of the directions of the company{`'`}s activity is research and development work on the search for the latest technologies and materials, which are embodied in the design of products. A lot of own developments of A.T.O.R. NGO LLC protected by a number of patents in the field of passenger rail transport.
       </p>
 
       <CertificatesAndPatents status={true}/>
